8d4aef331c1536ec5defff13c7827ceb7f62bcc1
Cole <col.j.stew@gmail.com>
2021-10-02 12:44:12 -0700

    modified PaperColorNamespace

--- components/sweep-table.tsx
+++ components/sweep-table.tsx
@@ -1,34 +1,45 @@
 import React from 'react';
 import { StyleSheet, View, ScrollView } from 'react-native';
-import { DataTable } from 'react-native-paper';
+import { DataTable, useTheme } from 'react-native-paper';
 import TableTabs from './table-tabs';
 
 interface Props{
   tabs? : boolean,
 }
-const SweepTable = ({ tabs = false } : Props) : JSX.Element => (
-  <>
-    {tabs && <TableTabs />}
-    <View style={style.tableView}>
-      <DataTable style={tabs ? [style.table, style.tableWithTabs] : style.table}>
-        <DataTable.Header>
-          <DataTable.Title>Species</DataTable.Title>
-          <DataTable.Title numeric>Count</DataTable.Title>
-        </DataTable.Header>
-        <ScrollView>
-          <DataTable.Row>
-            <DataTable.Cell>C 175</DataTable.Cell>
-            <DataTable.Cell numeric>2</DataTable.Cell>
-          </DataTable.Row>
-          <DataTable.Row>
-            <DataTable.Cell>F 575</DataTable.Cell>
-            <DataTable.Cell numeric>1</DataTable.Cell>
-          </DataTable.Row>
-        </ScrollView>
-      </DataTable>
-    </View>
-  </>
-);
+const SweepTable = ({ tabs = false } : Props) : JSX.Element => {
+  const theme = useTheme();
+  return (
+    <>
+      {tabs && <TableTabs />}
+      <View style={style.tableView}>
+        <DataTable style={[
+          style.table,
+          tabs && style.tableWithTabs,
+          {
+            borderColor: theme.colors.border,
+            borderRadius: theme.roundness,
+          },
+        ]}
+        >
+          <DataTable.Header>
+            <DataTable.Title>Species</DataTable.Title>
+            <DataTable.Title numeric>Count</DataTable.Title>
+          </DataTable.Header>
+          <ScrollView>
+            <DataTable.Row>
+              <DataTable.Cell>C 175</DataTable.Cell>
+              <DataTable.Cell numeric>2</DataTable.Cell>
+            </DataTable.Row>
+            <DataTable.Row>
+              <DataTable.Cell>F 575</DataTable.Cell>
+              <DataTable.Cell numeric>1</DataTable.Cell>
+            </DataTable.Row>
+          </ScrollView>
+        </DataTable>
+      </View>
+    </>
+  );
+};
 
 const style = StyleSheet.create({
   tableView: {
@@ -37,9 +48,7 @@ const style = StyleSheet.create({
   },
   table: {
     flex: 1,
-    borderWidth: 0.75,
-    borderRadius: 4, // match theme
-    borderColor: 'lightgray', // match theme
+    borderWidth: 0.3,
   },
   tableWithTabs: {
     borderTopLeftRadius: 0,
